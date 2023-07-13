c26bf197ac58136222a625ff23311cf3bb3af9c7
insdram <inminu@me.com>
2023-07-13 10:06:21 +0800

    123

--- api/index.py
+++ api/index.py
@@ -5,34 +5,30 @@ import os
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-def get_data(neo_type, neo_category):
+def get_data(neo_type, neo_category, neo_page):
     if neo_type not in ['wishlist', 'progress', 'complete']:
         raise ValueError('Invalid type parameter. Must be wishlist, progress, or complete')
     if neo_category not in ['book', 'movie', 'tv', 'music', 'game', 'podcast']:
         raise ValueError('Invalid category parameter. Must be book, movie, tv, music, game, or podcast')
     
-    url = f'https://neodb.social/api/me/shelf/{neo_type}?category={neo_category}'
+    all_results = []
+
+    url = f'https://neodb.social/api/me/shelf/{neo_type}?category={neo_category}&page={neo_page}'
     headers = {'Authorization': 'Bearer ' + os.environ.get('AUTHORIZATION'), 'Accept': 'application/json'}
     response = requests.get(url, headers=headers)
-    json_data = json.loads(response.text)
-    pages_value = json_data['pages']
-    
-    all_results = []
-    for page_num in range(1, pages_value+1):
-        requests_path = f'{url}&page={page_num}'
-        r = requests.get(requests_path, headers=headers)
-        result = r.json()
-        all_results.extend(result['data'])
+    result = response.json()
+    all_results.extend(result['data'])
     
     return all_results
-
+    
 class Handler(BaseHTTPRequestHandler): 
     def do_GET(self):
         path = self.path
         neo_type = re.findall(r'type=([^&]*)', path)[0]
         neo_category = re.findall(r'category=([^&]*)', path)[0]
+        neo_page = re.findall(r'page=([^&]*)', path)[0]
         try:    
-            data = get_data(neo_type, neo_category) 
+            data = get_data(neo_type, neo_category, neo_page) 
         except Exception as e:  
             self.send_error(500, str(e))
             return
